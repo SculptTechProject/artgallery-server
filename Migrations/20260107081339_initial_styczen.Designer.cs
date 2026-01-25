82f13dae53c3257f7a1d7e5fa82a9d98c3058856
SculptTechProject <150788324+SculptTechProject@users.noreply.github.com>
2026-01-25 18:14:55 +0100

    Add initial migration for database schema and configure Docker volume for local development

--- Migrations/20260107081339_initial_styczen.Designer.cs
+++ Migrations/20260107081339_initial_styczen.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using artgallery_server.Infrastructure;
 
@@ -10,9 +11,11 @@ using artgallery_server.Infrastructure;
 namespace artgallery_server.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260107081339_initial_styczen")]
+    partial class initial_styczen
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
